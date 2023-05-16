c739a7894f5bea7f5f1a4062a0b61925e91f8c32
rsdrew <rsdrew@umich.edu>
2023-05-15 23:13:50 -0400

    Added animations. Updated how code shows. Added favicon.

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 document.getElementById('ticket').addEventListener('click', (e) => {
     let button = e.target;
     button.innerHTML = 'LOADING';
-    setTimeout(() => {
-        button.classList.add('hidden');
-        document.getElementById('success').classList.remove('hidden');
-        button.addEventListener('click', () => {return false;});
-    }, 2000);
+    let x = 0;
+    let loadingAnimation = setInterval(() => {
+        let percent = x * .3;
+        x++;
+        button.style.background = `linear-gradient(to right, var(--dark-yellow) ${percent}%, var(--bright-yellow) ${percent}%)`;
+
+        if(percent >= 120) {
+            clearInterval(loadingAnimation);
+
+            let getReady = document.getElementById('get-ready');
+            let divider = document.getElementById('divider');
+
+            getReady.style.opacity = 0;
+            divider.style.opacity = 0;
+            button.style.opacity = 0;
+
+            setTimeout(() => {
+                getReady.classList.add('hidden');
+                button.classList.add('hidden');
+
+                let youreIn = document.getElementById('youre-in');
+                let code = document.getElementById('code');
+                
+                code.classList.remove('hidden');
+                youreIn.classList.remove('hidden');
+
+                setTimeout(() => {
+                    code.style.opacity = 1;
+                    divider.style.opacity = 1;
+                    youreIn.style.opacity = 1;
+                }, 500);
+            }, 500);
+        }
+    }, 1);
 });
